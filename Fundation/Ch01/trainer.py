f1e6117b1b69f3a8313157655e5ba65ec1fdbaca
이세현 <86671456+qsdcfd@users.noreply.github.com>
2022-06-22 15:02:31 +0900

    Add files via upload

---
+++ Fundation/Ch01/trainer.py
@@ -0,0 +1,93 @@
+from copy import deepcopy
+
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+class Trainer():
+
+    def __init__(self, model, optimizer, crit):
+        self.model = model
+        self.optimizer = optimizer
+        self.crit = crit #loss의미
+
+        super().__init__()
+
+    def _train(self, x, y, config):
+        self.model.train() #까먹지 말기
+
+        # Shuffle before begin.필수: 매 epoch마다 minibatch로 들어가기 위한 것
+        indices = torch.randperm(x.size(0), device=x.device) #뭐를 랜덤으로 셔플링하질!
+        x = torch.index_select(x, dim=0, index=indices).split(config.batch_size, dim=0)
+        y = torch.index_select(y, dim=0, index=indices).split(config.batch_size, dim=0)
+
+        total_loss = 0
+
+        for i, (x_i, y_i) in enumerate(zip(x, y)):
+            y_hat_i = self.model(x_i)
+            loss_i = self.crit(y_hat_i, y_i.squeeze()) #squeeze: (x,)로 만든다
+
+            # Initialize the gradients of the model.
+            self.optimizer.zero_grad()
+            loss_i.backward()
+
+            self.optimizer.step() #weight 한 번 업데이트
+
+            if config.verbose >= 2:
+                print("Train Iteration(%d/%d): loss=%.4e" % (i + 1, len(x), float(loss_i)))
+
+            # Don't forget to detach to prevent memory leak.
+            total_loss += float(loss_i)#float를 하지 않으면 tensor로 된다.
+
+        return total_loss / len(x)
+
+    def _validate(self, x, y, config):
+        # Turn evaluation mode on.
+        self.model.eval() #해놓아야 성능이 떨어지지 않음
+
+        # Turn on the no_grad mode to make more efficintly.
+        with torch.no_grad():
+            # Shuffle before begin.
+            indices = torch.randperm(x.size(0), device=x.device)
+            x = torch.index_select(x, dim=0, index=indices).split(config.batch_size, dim=0)
+            y = torch.index_select(y, dim=0, index=indices).split(config.batch_size, dim=0)
+
+            total_loss = 0
+
+            for i, (x_i, y_i) in enumerate(zip(x, y)):
+                y_hat_i = self.model(x_i)
+                loss_i = self.crit(y_hat_i, y_i.squeeze())
+
+                if config.verbose >= 2:
+                    print("Valid Iteration(%d/%d): loss=%.4e" % (i + 1, len(x), float(loss_i)))
+
+                total_loss += float(loss_i)
+
+            return total_loss / len(x)
+
+    def train(self, train_data, valid_data, config):
+        lowest_loss = np.inf
+        best_model = None
+
+        for epoch_index in range(config.n_epochs):
+            train_loss = self._train(train_data[0], train_data[1], config)
+            valid_loss = self._validate(valid_data[0], valid_data[1], config)
+
+            # You must use deep copy to take a snapshot of current best weights.
+            if valid_loss <= lowest_loss:
+                lowest_loss = valid_loss
+                best_model = deepcopy(self.model.state_dict())
+
+            print("Epoch(%d/%d): train_loss=%.4e  valid_loss=%.4e  lowest_loss=%.4e" % (
+                epoch_index + 1,
+                config.n_epochs,
+                train_loss,
+                valid_loss,
+                lowest_loss,
+            ))
+
+        # Restore to best model.
+        self.model.load_state_dict(best_model)
+
